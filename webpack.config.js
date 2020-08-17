d6d9c5ce7c84a72f7ba61c00fe18541c062a854b
Vitor Santos <vitorblsantos@gmail.com>
2020-08-16 23:20:03 -0300

    Fix build problemns

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
 	entry: path.resolve(__dirname, './client/index.js'),
 	output: {
-		path: path.resolve(__dirname, './dist/'),
+		path: path.resolve(__dirname, './dist/view/'),
 		filename: 'bundle.js',
 	},
 
