5443c9681ae9f2167005acdcc6deefb9dae31e3b
Vitor Santos <vitorblsantos@gmail.com>
2020-08-22 18:49:40 -0300

    Order imports

--- server/index.js
+++ server/index.js
@@ -2,20 +2,20 @@
 
 import 'dotenv/config';
 
+import { json } from 'body-parser';
 import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
 import { join } from 'path';
-import { json } from 'body-parser';
 
 import routes from './routes';
 
 const server = express();
 
 server.disable('x-powered-by')
-server.use(express.static(join(__dirname, 'view')));
-server.use(compression());
 server.use(cors());
+server.use(compression());
 server.use(json());
+server.use(express.static(join(__dirname, 'view')));
 server.use(routes);
 server.listen(process.env.PORT || 5000);
