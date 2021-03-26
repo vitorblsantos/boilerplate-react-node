6e94f735a4052bbaf3d62aa22ea724fe4f68115d
Vitor Santos <vitorblsantos@gmail.com>
2021-03-25 22:48:57 -0300

    release: v1.1.3

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,14 @@
 import { Router } from 'express'
+import { join } from 'path'
 
 const router = Router()
 
 router.get('/api', function (_, res) {
   res.status(200).json({ message: 'Boilerplate React Node' })
+})
+
+router.get('*', function (_, res) {
+  res.status(200).sendFile(join(__dirname, '..', '..', '/build/client/index.html'))
 })
 
 export default router
